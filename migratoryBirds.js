001f983447b03d78a51e6c28caf3307cfb08c640
Karthik shivaram <48910378+karthik96siddhu@users.noreply.github.com>
2025-01-05 11:06:51 +0000

    problem 3

---
+++ migratoryBirds.js
@@ -0,0 +1,8 @@
+/**
+ * Given an array of bird sightings where every element represents a bird type id, determine the id of the most frequently sighted type. 
+ * If more than 1 type has been spotted that maximum amount, return the smallest of their ids.
+ */
+
+// example
+// input : arr = [1,4,4,4,5,3], ouput = 4
+// input : arr = [1,2,3,4,5,4,3,2,1,3,4], output = 3
