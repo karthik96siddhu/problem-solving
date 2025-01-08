7600970326d8d487475baada2cc8f5c4500b7ac8
Karthik shivaram <48910378+karthik96siddhu@users.noreply.github.com>
2025-01-08 11:49:48 +0000

    problem 3 update

--- migratoryBirds.js
+++ migratoryBirds.js
@@ -6,3 +6,26 @@
 // example
 // input : arr = [1,4,4,4,5,3], ouput = 4
 // input : arr = [1,2,3,4,5,4,3,2,1,3,4], output = 3
+
+function migratoryBirds(arr) {
+    let currentLowestNumber = arr[0];
+    const occuranceCount = {
+        [arr[0]]: 1
+    };
+    for (let i = 1; i < arr.length; i++) {
+        const val = arr[i]
+        if (!occuranceCount[val]) {
+            occuranceCount[val] = 1;
+        } else {
+            occuranceCount[val]++;
+        }
+        if (occuranceCount[currentLowestNumber] === occuranceCount[val]) {
+            if (val < currentLowestNumber) {
+                currentLowestNumber = val;
+            }
+        } else if (occuranceCount[currentLowestNumber] < occuranceCount[val]) {
+            currentLowestNumber = val;
+        }
+    }
+    return currentLowestNumber
+}
